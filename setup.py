b9eccce7bcd5f161bef9385ae967f2d464dfe46a
Abdelwahab <abdelwahabamr769@gmail.com>
2026-03-01 14:52:31 +0200

    project structre and env is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abdelwahab Amr",
+    author_email="abdelwahabamr769@gmail.com",
+    packages=find_packages()
+)
